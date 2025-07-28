453e6e6460d862741e1a08cd14292c2ded2339e5
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2025-07-28 16:57:59 +0900

    Refactoring workflow

---
+++ .github/actions/validate-semver/validate-semver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/env bash
+
+VERSION=$1
+SEMVER_REGEX="^v([0-9]+)\.([0-9]+)\.([0-9]+)$"
+if [[ ! "${VERSION}" =~ ${SEMVER_REGEX} ]]; then
+  echo "Invalid version: ${VERSION}"
+  exit 1
+fi
+echo "Version is valid: ${VERSION}"
